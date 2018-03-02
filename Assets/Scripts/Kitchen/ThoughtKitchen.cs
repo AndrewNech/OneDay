0589a0b0d0b8ef9a5637c7b926e4c3a0db30c334
AndrewNech <Drew.Fenix@yandex.ru>
2018-03-02 16:43:20 +0300

    change kitchen

---
+++ Assets/Scripts/Kitchen/ThoughtKitchen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThoughtKitchen : MonoBehaviour {
+
+    private string[] Thoughts = { "Кто-то не экономит электричество!","Я не могу уйти, пока не выключу приборы.",
+        "Но я хочу кушать.", "Это здорово, но что тут кушать?","Может откроем ее?"};
+    string[,] StatusMeaning = { { "Проснулся", "Спал" }, { "Не встал", "" }, { "Встал", "Не играл" }, { "Играл", "Не читал" }, { "Читал", "" } };
+
+    protected int statusNumber = 0;
+
+
+
+    public void ChangeStatus()
+    {
+        if (statusNumber < 5)
+        { statusNumber++; }
+    }
+
+
+
+    public void ShowThought(int thoughtnumber)
+    {
+        gameObject.SetActive(true);
+        gameObject.transform.Find("Text").GetComponent<Text>().text = Thoughts[thoughtnumber];
+        StartCoroutine(CloseThough());
+    }
+    IEnumerator CloseThough()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
+    public int GetStatus()
+    {
+        return statusNumber;
+    }
+}
--- Assets/Scripts/Kitchen/microwave.cs
+++ Assets/Scripts/Kitchen/microwave.cs
@@ -4,16 +4,107 @@ using UnityEngine;
 
 public class microwave : MonoBehaviour {
 
+    bool clicked = false;
+    bool usingMicrowaveEmpty = false;
+    bool usingMicrowaveFull = false;
+
+    public Sprite[] status;
+    public Sprite[] useMicrowaveImg;
+
+    public float shiftY = 0;
+    private float startY;
+
+    public float shiftX = 0;
+    private float startX;
+
+    public GameObject thoughtPanel;
+    ThoughtKitchen thought;
+    
+
+    private void Start()
+    {
+        thought = thoughtPanel.GetComponent<ThoughtKitchen>();
+        startY = gameObject.transform.position.y;
+        startX = gameObject.transform.position.x;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
-        if (collision.gameObject.name == "plate" && gameObject.GetComponent<SpriteRenderer>().sprite.name=="microwave_open")
+        if (gameObject.GetComponent<SpriteRenderer>().sprite.name == "microwave_closed")
         {
-            print("In microwave");
+            thought.ShowThought(4);
         }
+        else if (collision.gameObject.GetComponent<SpriteRenderer>().sprite.name == "plate_4")
+        {
+            usingMicrowaveEmpty = true;
+            gameObject.GetComponent<SpriteRenderer>().sprite = useMicrowaveImg[2];
+            collision.gameObject.SetActive(false);
+        }
+        else if (collision.gameObject.GetComponent<SpriteRenderer>().sprite.name == "plate_4_full")
+        {
+            usingMicrowaveFull = true;
+            gameObject.GetComponent<SpriteRenderer>().sprite = useMicrowaveImg[6];
+            collision.gameObject.SetActive(false);
+        }
+       
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
         collision.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+  
+    public bool Clicked
+    {
+        get { return clicked; }
+    }
+
+    public void OnMouseOver()
+    {
+            if (clicked)
+                gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(3);
+            else
+                gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(1);
+    }
+    public void OnMouseExit()
+    {
+
+        if (clicked)
+            gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(2);
+        else
+            gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(0);
+    }
+    public void OnMouseDown()
+    {
+
+        clicked = !clicked;
+       
+        
+            if (clicked)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(2);
+                gameObject.transform.position = new Vector3(shiftX, shiftY, -3f);
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = ReturnImg(0);
+                gameObject.transform.position = new Vector3(startX, startY, -3f);
+            }
+        
+       
+    }
+    private Sprite ReturnImg(int numberImg)
+    {
+        if (usingMicrowaveEmpty)
+        {
+            return useMicrowaveImg[numberImg];
+        }
+        else if (usingMicrowaveFull)
+        {
+            return useMicrowaveImg[4 + numberImg];
+        }
+        else
+        {
+            return status[numberImg];
+        }
     }
 }
