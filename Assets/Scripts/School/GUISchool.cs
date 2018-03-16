e51ebbf960574e83064c419f3b9ab11e8d73cb18
AndrewNech <Drew.Fenix@yandex.ru>
2018-03-16 22:04:55 +0300

    Add school, add last scene.

---
+++ Assets/Scripts/School/GUISchool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUISchool : MonoBehaviour
+{
+
+    public GameObject helpPanel;
+
+    public GameObject thoughPanel;
+    public void GoHome()
+    {
+        if (Slider.countSlides >= 5)
+        {
+            SceneManager.LoadScene(5);
+        }
+        else
+        {
+            print(Slider.countSlides);
+            thoughPanel.SetActive(true);
+            StartCoroutine(ShowThought());
+        }
+    }
+    IEnumerator ShowThought()
+    {
+        yield return new WaitForSeconds(3);
+        thoughPanel.SetActive(false);
+
+    }
+    public void ViewHelpPanel()
+    {
+        helpPanel.SetActive(true);
+    }
+    public void CloseHelpPanel()
+    {
+        helpPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/School/Slider.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Slider : MonoBehaviour
+{
+
+    int numberOfSlide = 0;
+
+    public GameObject slider;
+
+    public Sprite[] slides;
+
+    public Text textOnLesson;
+
+    public static int countSlides = 0;
+
+    string[] TextOnSlides = {
+        "Использование энергосберегающих лампочек помогает экономить электричество.Правда-правда.",
+        "Во время дождя поменьше пользоваться электроприборами в помещении и не пользоваться ими на улице. Не рекомендую проверять это.",
+        "В холодильник нельзя ставить горячие предметы, а в микроволновку металлические. Им это не нравится.",
+        "Не нужно ставить холодильник рядом с плитой, последствия могут быть страшными.",
+        "Если окна будут чистыми, то нужно будет меньше тратить электричества, да и так красивее.",
+        "Выключайте электроприборы, если их не используете. Ну зачем им просто так работать?" };
+
+    private void Start()
+    {
+        ChangeSlides(numberOfSlide);
+    }
+    public void LeftButtonTap()
+    {
+        try
+        {
+            numberOfSlide--;
+            ChangeSlides(numberOfSlide);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            numberOfSlide = slides.Length-1;
+            ChangeSlides(numberOfSlide);
+        }
+
+    }
+    public void RightButtonTap()
+    {
+        try
+        {
+            numberOfSlide++;
+            ChangeSlides(numberOfSlide);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            numberOfSlide = 0;
+            ChangeSlides(numberOfSlide);
+        }
+    }
+    private void ChangeSlides(int numberOfSlide)
+    {
+        slider.GetComponent<SpriteRenderer>().sprite = slides[numberOfSlide];
+        textOnLesson.text = TextOnSlides[numberOfSlide];
+
+        if (numberOfSlide == 1 || numberOfSlide == 2 || numberOfSlide == 3 || numberOfSlide == 4|| numberOfSlide == 5 )
+        {
+            countSlides++;
+        }
+        
+    }
+}
